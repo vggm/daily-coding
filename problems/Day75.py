4725855fb0c3162c1f5733cd8f936dcc591d7adf
Victor Gonzalez Morales <victorgm2709@gmail.com>
2023-10-16 02:31:00 +0200

    Day75

--- problems/Day75.py
+++ problems/Day75.py
@@ -12,23 +12,25 @@ from Solver import TestSolver
 
 INF = 999999999999
 
-def longest_subsequence ( nums: list[int] ) -> int:
-  solution = [-INF]
-  bt(nums, solution)
-  return solution[0]
+
+def longest_subsequence(nums: list[int]) -> int:
+    solution = [-INF]
+    bt(nums, solution)
+    return solution[0]
 
 
-def bt ( nums: list[int], sol: list[int], last: int = -INF,  total: int = 0, e=0 ) -> None:
-  
-  for i in range(e, len(nums)):
-    opt = nums[i]
-    if opt > last:
-      sol[0] = max( sol[0], total + 1 )
-      bt( nums, sol, opt, total + 1, i + 1 )
+def bt(nums: list[int], sol: list[int], last: int = -INF,  total: int = 0, e=0) -> None:
+
+    for i in range(e, len(nums)):
+        opt = nums[i]
+        if opt > last:
+            sol[0] = max(sol[0], total + 1)
+            bt(nums, sol, opt, total + 1, i + 1)
 
 
 if __name__ == '__main__':
-  
-  solver = TestSolver()
-  
-  solver.solve( longest_subsequence, [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], expected=6 )
+
+    solver = TestSolver()
+
+    solver.solve(longest_subsequence, [
+                 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], expected=6)
